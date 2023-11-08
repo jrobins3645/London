af5654e90bcc77c7ec5d5616417b11adff77c7e5
jrobins3645 <jeff.h.robinson@gmail.com>
2023-11-08 14:18:12 -0500

    First commit

---
+++ London/TEST.swift
@@ -0,0 +1,18 @@
+//
+//  TEST.swift
+//  London
+//
+//  Created by Jeff on 11/8/23.
+//
+
+import SwiftUI
+
+struct TEST: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TEST()
+}
